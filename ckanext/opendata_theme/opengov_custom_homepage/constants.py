f2b3211c59a1dc9b62fc39c29c070db5c5d2b00c
Peter Vorman <pvorman@opengov.com>
2023-03-03 18:02:09 +0200

    Added new templates

--- ckanext/opendata_theme/opengov_custom_homepage/constants.py
+++ ckanext/opendata_theme/opengov_custom_homepage/constants.py
@@ -1,6 +1,8 @@
 LAYOUTS = [
     {"text": "Homepage layout 1", "value": 1},
     {"text": "Homepage layout 2", "value": 2},
+    {"text": "Homepage layout 3", "value": 3},
+    {"text": "Homepage layout 4", "value": 4},
 ]
 CONTROLLER = 'ckanext.opendata_theme.opengov_custom_homepage.plugin.pylons_plugin:CSSController'
 CUSTOM_NAMING = "ckanext.opendata_theme.custom_naming"
--- ckanext/opendata_theme/opengov_custom_homepage/plugin/__init__.py
+++ ckanext/opendata_theme/opengov_custom_homepage/plugin/__init__.py
@@ -18,6 +18,7 @@ class OpenDataThemeHomepagePlugin(MixinPlugin):
     # IConfigurer
     def update_config(self, ckan_config):
         toolkit.add_template_directory(ckan_config, '../templates')
+        toolkit.add_public_directory(ckan_config, '../public')
 
         if toolkit.check_ckan_version(min_version='2.4', max_version='2.9'):
             toolkit.add_ckan_admin_tab(ckan_config, 'custom_home_page', 'Home Page Layout')
@@ -38,6 +39,8 @@ class OpenDataThemeHomepagePlugin(MixinPlugin):
         return {
             'opendata_theme_get_dataset_count': helper.dataset_count,
             'opendata_theme_get_showcases': helper.showcases,
+            'opendata_theme_get_showcases_story': helper.showcase_story,
+            'opendata_theme_get_value_from_extras': helper.get_value_from_extras,
             'opendata_theme_get_groups': helper.groups,
             'opendata_theme_get_datasets_new': helper.new_datasets,
             'opendata_theme_get_datasets_popular': helper.popular_datasets,
