ee9c1a54524514cc5667ebd30579e8cd8369d9c2
Jay Guo <jguo144@gmail.com>
2023-10-16 23:27:35 -0400

    Add check for orgportals plugin

--- ckanext/opendata_theme/extended_themes/cnra/plugin.py
+++ ckanext/opendata_theme/extended_themes/cnra/plugin.py
@@ -1,6 +1,10 @@
 import json
+from logging import getLogger
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+
+log = getLogger(__name__)
 
 boolean_validator = toolkit.get_validator('boolean_validator')
 
@@ -11,10 +15,10 @@ def display_org(organization_name):
         result = toolkit.get_action('organization_show')(None, {'id': organization_name})
         extras = result.get('extras', {})
         for field in extras:
-            if field.get('key', '').lower() in ['hide','hidden'] and boolean_validator(field.get('value'), None):
+            if field.get('key', '').lower() in ['hide', 'hidden'] and boolean_validator(field.get('value'), None):
                 return False
-    except:
-        print('[opendata_theme] Error in retrieving metadata for organization {}'.format(organization_name))
+    except Exception:
+        log.error('Error in retrieving metadata for organization %s', organization_name)
     return True
 
 
@@ -23,7 +27,7 @@ def get_org_upload_url(org_id=''):
     urls = json.loads(url_list)
     for item in urls:
         if org_id == item.get('org_id'):
-            return item.get('upload_url','')
+            return item.get('upload_url', '')
     return ''
 
 
