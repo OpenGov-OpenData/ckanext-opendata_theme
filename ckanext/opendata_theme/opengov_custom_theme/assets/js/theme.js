65ea698a201f16cf4c4571d4eb6da48a736fda69
Jay Guo <jguo144@gmail.com>
2024-11-22 17:35:32 -0500

    Refactor trap focus in modal JS

--- ckanext/opendata_theme/opengov_custom_theme/assets/js/theme.js
+++ ckanext/opendata_theme/opengov_custom_theme/assets/js/theme.js
@@ -8,88 +8,85 @@ $(document).on('keydown', function (e) {
   }
 });
 
-this.ckan.module('resource-view-embed', function ($) {
-  var modal;
-  var self;
+// Trap focus inside api-info modal
+$(document).ready(function () {
+  $('a[data-module="api-info"]').on('click', function () {
+    trapApiInfoEmbed();
+  });
 
-  function initialize() {
-    self = this;
-    modal = $('#embed-'+this.options.id)
-    $('body').append(modal);
-    this.el.on('click', _onClick);
-    $('textarea', modal).on('focus', _selectAllCode).on('mouseup', _preventClick);
-    $('input', modal).on('keyup change', _updateValues);
-    _updateEmbedCode();
+  $('a[data-module="api-info"]').on('keyup', function (e) {
+    if (e.key === 'Enter') {
+      trapApiInfoEmbed();
+    }
+  });
 
-    // Trap focus when modal is shown
-    modal.on('shown.bs.modal', _trapFocus);
-    modal.on('hidden.bs.modal', _restoreFocus);
-  }
-
-  function _onClick (event) {
-    event.preventDefault();
-    modal.modal('show');
-  }
-
-  function _selectAllCode () {
-    $('textarea', modal).select();
-  }
-
-  function _updateValues () {
-    self.options.width = $('[name="width"]', modal).val();
-    self.options.height = $('[name="height"]', modal).val();
-    _updateEmbedCode();
-  }
-
-  function _updateEmbedCode () {
-    $('[name="code"]', modal).val(_embedCode());
-  }
-
-  function _preventClick (event) {
-    event.preventDefault();
-  }
-
-  function _embedCode () {
-    return '<iframe width="' + self.options.width + '" height="' + self.options.height + '" src="' + self.options.url + '" frameBorder="0"></iframe>';
-  }
-
-  // Trap focus
-  function _trapFocus() {
-    const focusableElements = modal.find('a, button, input, textarea');
-    const firstElement = focusableElements.first();
-    const lastElement = focusableElements.last();
-
-    modal.on('keydown', function (e) {
-      if (e.key === 'Tab') {
-        if (e.shiftKey) { // Shift + Tab
-          if (document.activeElement === firstElement[0]) {
-            e.preventDefault();
-            lastElement.focus();
-          }
-        } else { // Tab
-          if (document.activeElement === lastElement[0]) {
-            e.preventDefault();
-            firstElement.focus();
-          }
-        }
-      }
-    });
-
-    // Move focus to the modal
-    firstElement.focus();
-  }
-
-  function _restoreFocus() {
-    modal.off('keydown');
-  }
-
-  return {
-    initialize: initialize,
-    options: {
-      id: 0,
-      url: '#',
-      width: 700,
-      height: 400
+  function trapApiInfoEmbed() {
+    const modal = $(`#api-info-embed`);
+    if (modal.length) {
+      // Trap focus within the modal
+      trapFocus(modal);
+      // When closed remove event handlers
+      modal.find('.close').on('click', function () {
+        modal.off('keydown');
+        $('[data-module="api-info"]').focus();
+      });
     }
   }
 });
+
+// Trap focus inside resource-view-embed modal
+$(document).ready(function () {
+  $('a[data-module="resource-view-embed"]').on('click', function () {
+    const modalId = $(this).data('module-id');
+    trapResourceViewEmbed(modalId);
+  });
+
+  $('a[data-module="resource-view-embed"]').on('keyup', function (e) {
+    if (e.key === 'Enter') {
+      const modalId = $(this).data('module-id');
+      trapResourceViewEmbed(modalId);
+    }
+  });
+
+  function trapResourceViewEmbed(modalId) {
+    const modal = $(`#embed-${modalId}`);
+    if (modal.length) {
+      // Trap focus within the modal
+      trapFocus(modal);
+      // When closed remove event handlers
+      modal.find('.close').on('click', function () {
+        modal.off('keydown');
+        $('[data-module="resource-view-embed"]').focus();
+      });
+    }
+  }
+});
+
+function trapFocus(modal) {
+  const focusableElements = modal.find('a, button, input, textarea');
+  const firstElement = focusableElements.first();
+  const lastElement = focusableElements.last();
+
+  // Focus on first element when the modal opens
+  modal.on('shown.bs.modal', function () {
+    firstElement.focus();
+  });
+
+  modal.on('keydown', function (e) {
+    if (e.key === 'Tab') {
+      if (e.shiftKey) {
+        // Shift + Tab
+        if (document.activeElement === firstElement[0]) {
+          e.preventDefault();
+          lastElement.focus();
+        }
+      } else {
+        // Tab
+        if (document.activeElement === lastElement[0]) {
+          e.preventDefault();
+          firstElement.focus();
+        }
+      }
+    }
+  });
+}
